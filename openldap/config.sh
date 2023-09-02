c56bd39c1703f1f112d8e407a115ae9bd659159f
Lilian Schall <lilian.schall@epita.fr>
2023-09-03 01:43:58 +0200

    feat: transformed openldap docker image to be able to back a kerberos service

--- openldap/config.sh
+++ openldap/config.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+
+cat > /etc/krb5.conf << EOF
+[libdefaults]
+	default_realm = $KRB5_REALM
+	ignore_acceptor_hostname = true
+	rdns = false
+EOF
+
+if [ -n "$KRB5_KDC" ]; then
+	cat >> /etc/krb5.conf << EOF
+[realms]
+	$KRB5_REALM = {
+		kdc = $KRB5_KDC
+	}
+EOF
+
+fi
+
+cat >> /etc/krb5.conf << EOF
+[domain_realm]
+	$LDAP_DOMAIN = $KRB5_REALM
+	.$LDAP_DOMAIN = $KRB5_REALM
+EOF
--- openldap/docker-entrypoint.sh
+++ openldap/docker-entrypoint.sh
@@ -4,8 +4,10 @@ COPY_SERVICE=${COPY_SERVICE:-0}
 
 /container/config.sh
 
+[ "$#" -ne 0  ] && exec "$@"
+
 if [ "$COPY_SERVICE" = "1" ]; then
-    exec /container/tool/run --copy-service;
+    exec /container/tool/run --copy-service --loglevel debug;
 else
     exec /container/tool/run;
 fi
